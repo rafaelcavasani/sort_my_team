de302f6466ed9b9feeb2f0852134f9b0bf389e0c
Rafael Trindade Cavasani <rafael.cavasani@hotmail.com>
2019-10-04 12:34:30 -0300

    Update team_routes.py

--- routes/team_routes.py
+++ routes/team_routes.py
@@ -3,6 +3,5 @@ from flask import jsonify, request
 from services import team_service
 
 @app.route('/', methods=["POST"])
-@cross_origin(origin='*')
 def Index():
     return jsonify(team_service.sort_teams(request)), 200
